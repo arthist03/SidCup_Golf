5b1a1082fa7a5ec642ea8c228bd1e55b46ab50b0
Arth Kadiya <arthpk09@gmail.com>
2023-11-17 23:44:54 +0530

    Done All

--- script.js
+++ script.js
@@ -68,7 +68,6 @@ gsap.from("#about-us img, #about-us-in",{
 y:90,
 opacity: 0,
 duration:1,
-stagger:0.4,
 scrollTrigger:{
   trigger:"#about-us",
   scroller:"body",
@@ -81,11 +80,47 @@ gsap.from(".card",{
 scale:0.8,
 opacity: 0,
 duration:1,
+stagger:0.1,
 scrollTrigger:{
   trigger:".card",
   scroller:"body",
-  start:"top 60%",
-  end: "top 50%",
+  start:"top 75%",
+  end: "top 70%",
   scrub:1
 }
 })
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#page4 h1", {
+  y: 50,
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    // markers:true,
+    start: "top 75%",
+    end: "top 70%",
+    scrub: 3,
+  },
+});
